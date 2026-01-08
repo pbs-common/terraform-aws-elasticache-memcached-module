e1bd3498e12d0c2122ff8fac86248dde110514c2
Cristian Cioltan <cristian.cioltan@3pillarglobal.com>
2026-01-08 16:00:37 +0200

    CAT-26069 - Upgrading the terraform module

--- tags.tf
+++ tags.tf
@@ -34,6 +34,17 @@ variable "organization" {
   }
 }
 
+variable "owner" {
+  description = "Tag used to group resources according to owner"
+  type        = string
+  default     = "plops"
+
+  validation {
+    condition     = can(regex("[a-z\\-]+", var.owner))
+    error_message = "The owner variable violates approved regex."
+  }
+}
+
 variable "tags" {
   description = "Extra tags"
   default     = {}
